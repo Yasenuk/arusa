b448394b7da4bdd8d660963cef1fb6e9e9120e4c
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-05-07 01:53:37 +0300

    feat: add npRequest function for Novaposhta API interaction

---
+++ libs/utils/src/lib/novaposhta.ts
@@ -0,0 +1,10 @@
+export async function npRequest(modelName: string, calledMethod: string, methodProperties: object = {}) {
+  const res = await fetch('/api/novaposhta', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ modelName, calledMethod, methodProperties })
+  });
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.error);
+  return data;
+}
