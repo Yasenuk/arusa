303fe37cba9fd0483f5bc372f105e480d5fafddd
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-05-07 01:05:58 +0300

    feat: add novaposhta API request utility function

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -3,6 +3,8 @@ export * from './lib/auth.js';
 export * from './lib/_list.js';
 export * from './lib/cart.js';
 
+export * from './lib/novaposhta.js'
+
 export * from './dom/device.js';
 export * from './dom/page-load.js';
 export * from './dom/webp.js';
