1a21bc3c848a042ea4728bf814be28c54c2a0655
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-05-07 01:48:17 +0300

    refactor: clean up tsconfig files by removing unnecessary includes

--- apps/shop/src/app/pages/user/sections/Addresses.tsx
+++ apps/shop/src/app/pages/user/sections/Addresses.tsx
@@ -1,12 +1,20 @@
 
+import { use, useEffect, useState } from 'react';
+
 import styles from '../profile.module.scss';
 
+import { npRequest } from '@org/utils/index';
+
 export default function Addresses() {
+	const [addresses, setAddresses] = useState([]);
+
+	useEffect(() => {
+		
+	}, []);
+
 	return (
 		<section className={styles.profile__section}>
 			<h2 className={`${styles.profile__title} large upper`}>Адреси</h2>
-
-			
 		</section>
 	);
 }
