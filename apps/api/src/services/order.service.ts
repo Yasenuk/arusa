5bf05accbd5bc914e427a670e27f28334aadf326
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-05-07 22:53:23 +0300

    feat (order.service): add createOrder functionality

---
+++ apps/api/src/services/order.service.ts
@@ -0,0 +1,79 @@
+import { prisma } from "../db/prisma";
+import { getCart } from "./cart.service";
+
+export async function createOrder(user_id: number, address_id?: number) {
+	return await prisma.$transaction(async (tx) => {
+		const cart = await tx.carts.findUnique({
+			where: { user_id },
+			include: {
+				cart_items: {
+					include: {
+						product_variants: {
+							include: {
+								products: true
+							}
+						}
+					}
+				}
+			}
+		});
+
+		if (!cart || cart.cart_items.length === 0) {
+			throw new Error("Кошик порожній");
+		}
+
+		for (const item of cart.cart_items) {
+			const inventory = await tx.inventory.findFirst({
+				where: { product_variant_id: item.product_variant_id }
+			});
+
+			const avalible = (inventory?.quantity || 0) - (inventory?.reserved_quantity || 0);
+
+			if (avalible < item?.quantity) {
+				throw new Error(
+					`Недостатньо товару id[${item.product_variant_id}] (${avalible})`
+				);
+			}
+		}
+
+		const total_amount = cart.cart_items.reduce((sum, item) => {
+			return sum + Number(item.product_variants.price) * item.quantity
+		}, 0);
+
+		const order = tx.orders.create({
+			data: {
+				user_id,
+				address_id: address_id ?? null,
+				status: "PENDING_CONFIRMATION",
+				total_amount,
+				currency: "грн",
+				order_items: {
+					create: cart.cart_items.map((item) => ({
+						product_variants: item.product_variant_id,
+						price_snapshot: item.product_variants.price ?? 0,
+						title_snapshot: `Variant ${item.product_variant_id}`,
+						quantity: item.quantity
+					}))
+				}
+			},
+			include: {
+				order_items: true
+			}
+		});
+
+		for (const item of cart.cart_items) {
+			await tx.inventory.updateMany({
+				where: { product_variant_id: item.product_variant_id },
+				data: {
+					reserved_quantity: item.quantity
+				}
+			})
+		}
+
+		await tx.cart_items.deleteMany({
+			where: { cart_id: cart.id }
+		});
+
+		return order;
+	});
+}
--- apps/shop/src/app/pages/user/sections/Addresses.tsx
+++ apps/shop/src/app/pages/user/sections/Addresses.tsx
@@ -6,12 +6,6 @@ import styles from '../profile.module.scss';
 import { npRequest } from '@org/utils/index';
 
 export default function Addresses() {
-	const [addresses, setAddresses] = useState([]);
-
-	useEffect(() => {
-		
-	}, []);
-
 	return (
 		<section className={styles.profile__section}>
 			<h2 className={`${styles.profile__title} large upper`}>Адреси</h2>
